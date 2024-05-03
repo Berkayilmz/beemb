7df65f42552ca168464e603281c70808f8231675
emrearcnn <emrearcnn@outlook.com>
2024-05-03 23:41:52 +0300

    Mehmet Emre Arican

---
+++ client/src/components/Campaigns/campaings.jsx
@@ -0,0 +1,21 @@
+import campaignItem from "./campaingItem";
+import "./campaigns.css"
+
+const campaigns = () => {
+  return (
+    <section className="campaigns">
+      <div className="container">
+        <div className="campaigns-wrapper">
+          <campaignItem />
+          <campaignItem />
+        </div>
+        <div className="campaigns-wrapper">
+          <campaignItem />
+          <campaignItem />
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default campaigns;
