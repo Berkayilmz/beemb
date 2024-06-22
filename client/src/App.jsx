367fecb1cbe69af3a7ac68ba95466ccb69b6ff0a
emrearcnn <emrearcnn@outlook.com>
2024-06-22 03:30:45 +0300

    Mehmet Emre Arican

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -14,7 +14,12 @@ import UserPage from "./pages/admin/UserPage";
 import CategoryPage from "./pages/admin/Categories/CategoryPage";
 import UpdateCategoryPage from "./pages/admin/Categories/UpdateCategoryPage";
 import CreateCategoryPage from "./pages/admin/Categories/CreateCategoryPage";
-
+import CreateProductPage from "./pages/admin/Products/CreateProductPage";
+import ProductPage from "./pages/admin/Products/ProductPage";
+import UpdateProductPage from "./pages/admin/Products/UpdateProductPage";
+import CouponPage from "./pages/admin/Coupons/CouponPage";
+import CreateCouponPage from "./pages/admin/Coupons/CreateCouponPage";
+import UpdateCouponPage from "./pages/admin/Coupons/UpdateCouponPage";
 
 const App = () => {
   return (
@@ -28,10 +33,20 @@ const App = () => {
         <Route path="/auth" element={<AuthPage />} />
         <Route path="/product/:id" element={<ProductDetailsPage />} />
         <Route path="/admin/*">
-          <Route path="users" element={<UserPage/>}/>
-          <Route path="categories" element={<CategoryPage/>}/>
-          <Route path="categories/update/:id" element={<UpdateCategoryPage />} />
+          <Route path="users" element={<UserPage />} />
+          <Route path="categories" element={<CategoryPage />} />
+          <Route
+            path="categories/update/:id"
+            element={<UpdateCategoryPage />}
+          />
+
           <Route path="categories/create" element={<CreateCategoryPage />} />
+          <Route path="products" element={<ProductPage />} />
+          <Route path="products/create" element={<CreateProductPage />} />
+          <Route path="products/update/:id" element={<UpdateProductPage />} />
+          <Route path="coupons" element={<CouponPage />} />
+          <Route path="coupons/create" element={<CreateCouponPage />} />
+          <Route path="coupons/update/:id" element={<UpdateCouponPage />} />
         </Route>
       </Routes>
     </>
