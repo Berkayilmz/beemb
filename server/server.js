9a7d63d8add34a3d3284769c32a94c6a9051cdae
Berkayilmz <berkayilmz01@gmail.com>
2024-06-27 23:42:43 +0300

    f

--- server/server.js
+++ server/server.js
@@ -2,36 +2,35 @@ const express = require('express');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const logger = require('morgan');
+const cors = require('cors');
 
 const app = express();
 const mainRoute = require('./routes/index.js');
-const PORT = 3000;
-const cors = require('cors');
+const PORT = process.env.PORT || 3000;  // Use environment variable PORT
 
 dotenv.config();
 
 const MongoDBConnect = async () => {
     try {
-        await mongoose.connect(process.env.MONGO_URI);
+        await mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
         console.log('Connected to MongoDB!');
     } catch (error) {
         console.error(error);
     }
 }
 
-//middlewares 
+// Middlewares 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(cors({
-    origin: 'http://localhost:5173', 
-    methods: ['GET', 'POST', 'DELETE', 'PUT'], 
-    credentials: true 
-  }));
-  
+    origin: process.env.FRONTEND_URL,  // Use environment variable for frontend URL
+    methods: ['GET', 'POST', 'DELETE', 'PUT'],
+    credentials: true
+}));
 
 app.use('/api', mainRoute);
 
 app.listen(PORT, () => {
     MongoDBConnect();
-    console.log(`PORT ${PORT} listening!`);
-})
+    console.log(`Server listening on port ${PORT}`);
+});
