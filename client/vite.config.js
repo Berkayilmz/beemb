a0585f989e297cd54d112a33f101d5ed3edc25a2
Berkayilmz <berkayilmz01@gmail.com>
2024-06-28 06:16:32 +0300

    f

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,13 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: 'dist',
-    rollupOptions: {
-      external: ['antd']
-    }
-  },
-  server: {
-    historyApiFallback: true
+  optimizeDeps: {
+    exclude: ['js-big-decimal']
   }
 })
