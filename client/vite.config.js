f9ea3d8832e9d7a872db7e9ee1224a03826df73f
Berkayilmz <berkayilmz01@gmail.com>
2024-06-28 03:23:24 +0300

    f

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,10 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  optimizeDeps: {
-    exclude: ['js-big-decimal']
-  },
   build: {
-    outDir: '/client/public/img'
+    outDir: 'dist'
   }
 })
